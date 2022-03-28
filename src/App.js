a45e21bb0fc1a6244f07b0c4aae3bd38d70684cd
MirzaabdullayevDev <mirzaabdullayevtest@gmail.com>
2022-03-28 18:06:36 +0500

    Theory part-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,103 @@
+import { Component } from 'react'
 import Car from './componenets/Car'
 
-function App() {
-    const divStyle = {
-        display: 'flex',
-        justifyContent: 'center'
+class App extends Component {
+    state = {
+        cars: [
+            { name: 'Ford', year: 1990, color: 'red' },
+            { name: 'BMW', year: 2020, color: 'black' },
+            { name: 'Mazda', year: 2022, color: 'blue' },
+        ],
+        title: 'React components',
+        val: null,
+        show: true,
+        hasError: false
     }
-    return (
-        <div className="cars" style={divStyle}>
-            <Car name="Ford" year="2020" />
-            <Car name="Audi" year="2019" />
-            <Car name="BMW" year="2022" />
-            <Car name="Tesla" year="2022"></Car>
-        </div >
-    )
+
+    /* ============= Handler functions started */
+    changeTitleHandler = () => {
+        this.setState({ title: this.state.val })
+    }
+
+    changeInputValue = (e) => {
+        this.setState({ val: e.target.value })
+    }
+
+    changeBG = (index) => {
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        let rgb = `rgb(${r}, ${g}, ${b})`
+
+        // let cars = this.state.cars.concat()
+
+        let cars = [...this.state.cars]
+        cars[index].color = rgb
+        this.setState({ cars })
+    }
+
+    changeComponentValue = (name, index) => {
+        let cars = [...this.state.cars]
+        cars[index].name = name
+
+        if (name.length <= 0) {
+            this.setState({ hasError: true })
+        } else {
+            this.setState({ hasError: false })
+        }
+
+        this.setState({ cars })
+    }
+
+    toggleHandler = () => {
+        this.setState({ show: !this.state.show })
+    }
+
+    removeHandler = (idx) => {
+        let cars = this.state.cars.concat() // 
+        cars.splice(idx, 1)
+        this.setState({ cars })
+    }
+
+    /* ============= Handler functions started */
+
+    render() {
+        const divStyle = {
+            display: 'flex',
+            justifyContent: 'center'
+        }
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>{this.state.title}</h1>
+                <input type="text" onChange={this.changeInputValue} />
+                <input type="submit" value="Change title" onClick={() => { this.changeTitleHandler() }} />
+
+                <button onClick={this.toggleHandler}>Show/Hide</button>
+                {
+                    this.state.show ?
+                        <div className="cars" style={divStyle}>
+                            {this.state.cars.map((car, index) => {  /* key bo'lishi kerak */
+                                return (
+                                    <Car
+                                        hasError={this.state.hasError}
+                                        key={index}
+                                        idx={index}
+                                        name={car.name}
+                                        year={car.year}
+                                        color={car.color}
+                                        onClickHandler={this.changeTitleHandler}
+                                        changeBgHandler={this.changeBG}
+                                        changeComponentValue={this.changeComponentValue}
+                                        removeHandler={this.removeHandler}
+                                    />
+                                )
+                            })}
+                        </div >
+                        : null
+                }
+            </div>
+        )
+    }
 }
 
 export default App
--- src/componenets/Car.js
+++ src/componenets/Car.js
@@ -1,100 +1,43 @@
-import { Component } from 'react'
-import './Car.css'
+import cls from './Car.module.css'
 
-class Car extends Component {
-    state = {
-        name: 'Ford',
-        year: 2022,
-        background: 'indigo',
-        opacity: null,
+function Car(props) {
+    const classes = [cls.car]
+    const inputCls = []
+
+    const divStyle = {
+        background: props.color,
     }
 
-    constructor(props) {
-        super()
-        this.name = props.name
-        this.year = props.year
+    if (props.hasError) {
+        inputCls.push(cls.error)
+    } else {
+        inputCls.push(cls.success)
     }
 
-    render() {
-        const divStyle = {
-            'textAlign': 'center',
-            margin: '10px',
-            border: '1px solid #ccc',
-            boxShadow: '0 0 8px #ccc',
-            padding: '10px 50px',
-            display: 'inline-block',
-            cursor: 'pointer',
-            transition: ".5s",
-            background: this.state.background,
-            color: 'white'
-        }
+    return (
+        <div className={classes.join(' ')} style={divStyle}>
+            <h2>{props.name}</h2>
+            <input
+                className={inputCls.join(' ')}
+                type="text"
+                onChange={(e) => { props.changeComponentValue(e.target.value, props.idx) }}
+                value={props.name}
+            />
 
-        const func = () => {
-            let r = Math.floor(Math.random() * 256)
-            let g = Math.floor(Math.random() * 256)
-            let b = Math.floor(Math.random() * 256)
-            let rgb = `rgb(${r}, ${g}, ${b})`
+            <p><strong>Year: </strong>{props.year}</p>
 
-            console.log(rgb);
+            <button
+                // onClick={() => props.changeBgHandler(props.idx)}
+                onClick={props.changeBgHandler.bind(this, props.idx)}
+                style={{ cursor: 'pointer' }}
+            >RGB
+            </button>
 
-            this.setState({ background: rgb })
-        }
-
-        return (
-            <div className='car' style={divStyle}>
-                {/* <h2 style={{ color: 'red', fontSize: '30px' }}>{this.name}</h2>
-                <p><strong>Year:</strong> {this.year}</p>
-                <p><strong>Age: </strong> {2022 - this.year}</p>
-                <p><strong>Id: </strong> {Math.floor(Math.random() * 100)}</p> */}
-
-                <h2>{this.state.name}</h2>
-                <p><strong>Year: </strong>{this.state.year}</p>
-                <button onClick={func} style={{ cursor: 'pointer' }}>RGB </button>
-            </div>
-        )
-    }
+            <button onClick={props.removeHandler.bind(this, props.idx)}>
+                Remove
+            </button>
+        </div>
+    )
 }
-
-// function Car(props) {
-//     const divStyle = {
-//         'textAlign': 'center',
-//         margin: '10px',
-//         border: '1px solid #ccc',
-//         boxShadow: '0 0 8px #ccc',
-//         padding: '10px 50px',
-//         display: 'inline-block',
-//         cursor: 'pointer',
-//         transition: ".5s"
-//     }
-
-
-//     return (
-//         <div className='car' style={divStyle}>
-//             <h2 style={{ color: 'red', fontSize: '30px' }}>{props.name}</h2>
-//             <p><strong>Year:</strong> {props.year}</p>
-//             <p><strong>Age: </strong> {2022 - props.year}</p>
-//             <p><strong>Id: </strong> {Math.floor(Math.random() * 100)}</p>
-//         </div>
-//     )
-// }
-
-// const Car = ({ name, year }) => {
-//     return (
-//         <div className='car'>
-//             <h2>{name}</h2>
-//             <p><strong>Year:</strong> {year}</p>
-//         </div>
-//     )
-// }
-
-// export default Car
-// export default function Car({ name, year }) {
-//     return (
-//         <div className='car'>
-//             <h2>{name}</h2>
-//             <p><strong>Year:</strong> {year}</p>
-//         </div>
-//     )
-// }
 
 export default Car
